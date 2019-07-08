f04d2d23e79e43cf57b63340789c815f2229a6ec
vinaytiwari <vinaytiwari264@gmail.com>
2019-07-08 11:27:00 +0530

    Project Initialize

---
+++ OLX.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OLX.Web.Startup))]
+namespace OLX.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
